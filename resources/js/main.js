c0870fe907f696d5f7f9e7d337e136ea45c5d5d9
Lcpires7 <133696287+Lcpires7@users.noreply.github.com>
2023-11-13 13:12:06 -0300

    Update

---
+++ resources/js/main.js
@@ -0,0 +1,306 @@
+let biblia = {
+    'genesis' : {
+        'genesis1' : {
+            'capitulo' : 1,
+            'versiculos' : 31,
+            'caracteres' : 726,
+            'statusLeitura' : 'pendente'
+    },
+    'genesis2' : {
+                'capitulo' : 2,
+                'versiculos' : 25,
+                'caracteres' : 574,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis3' : {
+                'capitulo' : 3,
+                'versiculos' : 24,
+                'caracteres' : 588,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis4' : {
+                'capitulo' : 4,
+                'versiculos' : 26,
+                'caracteres' : 596,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis5' : {
+                'capitulo' : 5,
+                'versiculos' : 32,
+                'caracteres' : 495,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis6' : {
+                'capitulo' : 6,
+                'versiculos' : 22,
+                'caracteres' : 525,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis7' : {
+                'capitulo' : 7,
+                'versiculos' : 24,
+                'caracteres' : 553,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis8' : {
+                'capitulo' : 8,
+                'versiculos' : 22,
+                'caracteres' : 531,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis9' : {
+                'capitulo' : 9,
+                'versiculos' : 29,
+                'caracteres' : 612,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis10' : {
+                'capitulo' : 10,
+                'versiculos' : 32,
+                'caracteres' : 465,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis11' : {
+                'capitulo' : 11,
+                'versiculos' : 32,
+                'caracteres' : 588,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis12' : {
+                'capitulo' : 12,
+                'versiculos' : 20,
+                'caracteres' : 466,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis13' : {
+                'capitulo' : 13,
+                'versiculos' : 18,
+                'caracteres' : 417,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis14' : {
+                'capitulo' : 14,
+                'versiculos' : 24,
+                'caracteres' : 571,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis15' : {
+                'capitulo' : 15,
+                'versiculos' : 21,
+                'caracteres' : 405,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis16' : {
+                'capitulo' : 16,
+                'versiculos' : 16,
+                'caracteres' : 384,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis17' : {
+                'capitulo' : 17,
+                'versiculos' : 27,
+                'caracteres' : 626,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis18' : {
+                'capitulo' : 18,
+                'versiculos' : 33,
+                'caracteres' : 754,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis19' : {
+                'capitulo' : 19,
+                'versiculos' : 38,
+                'caracteres' : 988,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis20' : {
+                'capitulo' : 20,
+                'versiculos' : 18,
+                'caracteres' : 453,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis21' : {
+                'capitulo' : 21,
+                'versiculos' : 34,
+                'caracteres' : 668,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis22' : {
+                'capitulo' : 22,
+                'versiculos' : 24,
+                'caracteres' : 558,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis23' : {
+                'capitulo' : 23,
+                'versiculos' : 20,
+                'caracteres' : 469,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis24' : {
+                'capitulo' : 24,
+                'versiculos' : 67,
+                'caracteres' : 1634,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis25' : {
+                'capitulo' : 25,
+                'versiculos' : 34,
+                'caracteres' : 649,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis26' : {
+                'capitulo' : 26,
+                'versiculos' : 35,
+                'caracteres' : 775,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis27' : {
+                'capitulo' : 27,
+                'versiculos' : 46,
+                'caracteres' : 1133,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis28' : {
+                'capitulo' : 28,
+                'versiculos' : 22,
+                'caracteres' : 538,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis29' : {
+                'capitulo' : 29,
+                'versiculos' : 35,
+                'caracteres' : 757,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis30' : {
+                'capitulo' : 30,
+                'versiculos' : 43,
+                'caracteres' : 906,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis31' : {
+                'capitulo' : 31,
+                'versiculos' : 55,
+                'caracteres' : 1300,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis32' : {
+                'capitulo' : 32,
+                'versiculos' : 32,
+                'caracteres' : 709,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis33' : {
+                'capitulo' : 33,
+                'versiculos' : 20,
+                'caracteres' : 464,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis34' : {
+                'capitulo' : 34,
+                'versiculos' : 31,
+                'caracteres' : 686,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis35' : {
+                'capitulo' : 35,
+                'versiculos' : 29,
+                'caracteres' : 599,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis36' : {
+                'capitulo' : 36,
+                'versiculos' : 43,
+                'caracteres' : 830,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis37' : {
+                'capitulo' : 37,
+                'versiculos' : 36,
+                'caracteres' : 803,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis38' : {
+                'capitulo' : 38,
+                'versiculos' : 30,
+                'caracteres' : 740,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis39' : {
+                'capitulo' : 39,
+                'versiculos' : 23,
+                'caracteres' : 588,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis40' : {
+                'capitulo' : 40,
+                'versiculos' : 23,
+                'caracteres' : 517,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis41' : {
+                'capitulo' : 41,
+                'versiculos' : 57,
+                'caracteres' : 1257,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis42' : {
+                'capitulo' : 42,
+                'versiculos' : 38,
+                'caracteres' : 851,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis43' : {
+                'capitulo' : 43,
+                'versiculos' : 34,
+                'caracteres' : 829,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis44' : {
+                'capitulo' : 44,
+                'versiculos' : 34,
+                'caracteres' : 780,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis45' : {
+                'capitulo' : 45,
+                'versiculos' : 28,
+                'caracteres' : 698,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis46' : {
+                'capitulo' : 46,
+                'versiculos' : 34,
+                'caracteres' : 701,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis47' : {
+                'capitulo' : 47,
+                'versiculos' : 31,
+                'caracteres' : 900,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis48' : {
+                'capitulo' : 48,
+                'versiculos' : 22,
+                'caracteres' : 609,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis49' : {
+                'capitulo' : 49,
+                'versiculos' : 33,
+                'caracteres' : 684,
+                'statusLeitura' : 'pendente'
+    },
+    'genesis50' : {
+                'capitulo' : 50,
+                'versiculos' : 26,
+                'caracteres' : 622,
+                'statusLeitura' : 'pendente'
+    }
+    }
+}
+
+console.log(biblia.genesis.genesis1.versiculos)
